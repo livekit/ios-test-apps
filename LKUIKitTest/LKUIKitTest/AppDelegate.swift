e6acd18df3b512ab69d41533f00ef0f852aab04b
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2021-10-14 04:26:53 +0900

    update for new delegates

--- LKUIKitTest/LKUIKitTest/AppDelegate.swift
+++ LKUIKitTest/LKUIKitTest/AppDelegate.swift
@@ -8,33 +8,31 @@
 import Logging
 import UIKit
 import WebRTC
+import LiveKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+
     func logFactory(label: String) -> LogHandler {
         var handler = StreamLogHandler.standardError(label: label)
         handler.logLevel = .debug
         return handler
     }
 
-    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_: UIApplication,
+                     didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Customize logging. LiveKit uses SwiftLog
         LoggingSystem.bootstrap(logFactory)
-//        RTCSetMinDebugLogLevel(RTCLoggingSeverity.warning)
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options _: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    func application(_: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options _: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_: UIApplication, didDiscardSceneSessions _: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 }
--- LKUIKitTest/LKUIKitTest/RoomViewController.swift
+++ LKUIKitTest/LKUIKitTest/RoomViewController.swift
@@ -14,20 +14,20 @@ class RoomViewController: UIViewController {
     var remoteVideo: VideoView?
     var localVideo: VideoView?
 
-    var isRearCamera: Bool = false;
+    var isRearCamera: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.toggleCamera(_:))))
 
+        let url: String = "wss://xxxxxxx"
+        // swiftlint:disable:next line_length
+        let token: String = "xxxxxxxx"
 
-        let url: String = "ws://<your_host>"
-        let token: String = "<your_token>"
-        
         room = LiveKit.connect(options: ConnectOptions(url: url, token: token), delegate: self)
     }
-    
+
     override func viewDidDisappear(_ animated: Bool) {
         room?.disconnect()
         room = nil
@@ -40,7 +40,7 @@ class RoomViewController: UIViewController {
         let track = pub?.track as? LocalVideoTrack
 
         var options = LocalVideoTrackOptions()
-        options.captureParameter = VideoPreset.hd.capture
+        options.captureParameter = VideoParameters.presetHD169
         options.position = isRearCamera ? .front: .back
         isRearCamera = !isRearCamera
 
@@ -50,15 +50,33 @@ class RoomViewController: UIViewController {
 }
 
 extension RoomViewController: RoomDelegate {
+
     /* Room Delegate Methods */
-    func didConnect(room: Room) {
+    func room(_ room: Room, didConnect isReconnect: Bool) {
+        print("[Publish] attempt publish local tracks")
+
         if let localParticipant = room.localParticipant {
+
+            if isReconnect {
+                print("[Publish] ignoring publish since it is reconnect")
+                return
+            }
+
             DispatchQueue.global(qos: .background).async {
                 do {
                     var videoOpts = LocalVideoTrackOptions()
-                    videoOpts.captureParameter = VideoPreset.hd.capture
+                    videoOpts.captureParameter = VideoParameters.presetHD43
+
+                    var publishOptions = LocalVideoTrackPublishOptions()
+                    publishOptions.simulcast = true
+
                     let videoTrack = try LocalVideoTrack.createTrack(name: "localVideo", options: videoOpts)
-                    _ = localParticipant.publishVideoTrack(track: videoTrack)
+                    localParticipant.publishVideoTrack(track: videoTrack, options: publishOptions).then({ _ in
+                        print("[Publish] video success")
+                    }).catch({ error in
+                        print("[Publish] error \(error)")
+                    })
+
                 } catch {
                     print("\(error)")
                 }
@@ -69,39 +87,40 @@ extension RoomViewController: RoomDelegate {
         }
     }
 
-    func isReconnecting(room _: Room) {
-        print("room delegate --- reconnecting")
-    }
-
-    func didReconnect(room _: Room) {
-        print("room delegate --- reconnected")
-    }
-
-    func didDisconnect(room _: Room, error _: Error?) {
+    func room(_ room: Room, didDisconnect error: Error?) {
         print("room delegate --- did disconnect")
         DispatchQueue.main.async {
             self.dismiss(animated: true)
         }
     }
 
-    func didFailToConnect(room _: Room, error _: Error) {
-        print("room delegate --- did fail to connect")
+    //    func room(_ room: Room, didFailToConnect error: Error) {
+    //        print("room delegate --- did fail to connect")
+    //        DispatchQueue.main.async {
+    //            self.dismiss(animated: true)
+    //        }
+    //    }
+
+    func room(_ room: Room, participant: RemoteParticipant, didUnsubscribe trackPublication: RemoteTrackPublication) {
+        print("room delegate --- didUnsubscribe")
         DispatchQueue.main.async {
-            self.dismiss(animated: true)
+            self.remoteVideo?.removeFromSuperview()
+            self.remoteVideo = nil
         }
     }
 
-    func didSubscribe(track: Track, publication _: RemoteTrackPublication, participant _: RemoteParticipant) {
+    func room(_ room: Room, participant: RemoteParticipant,
+              didSubscribe trackPublication: RemoteTrackPublication, track: Track) {
         print("room delegate --- didSubscribe")
 
         if let videoTrack = track as? VideoTrack {
             DispatchQueue.main.async {
                 let videoView = VideoView(frame: .zero)
-                videoView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.toggleCamera(_:))))
+                videoView.addGestureRecognizer(UITapGestureRecognizer(target: self,
+                                                                      action: #selector(self.toggleCamera(_:))))
 
                 self.remoteVideo = videoView
                 videoView.translatesAutoresizingMaskIntoConstraints = false
-
 
                 if let localVideo = self.localVideo {
                     self.view.insertSubview(videoView, belowSubview: localVideo)
@@ -113,14 +132,11 @@ extension RoomViewController: RoomDelegate {
                     videoView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
                     videoView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
                     videoView.topAnchor.constraint(equalTo: self.view.topAnchor),
-                    videoView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+                    videoView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
                 ])
 
                 videoTrack.addRenderer(videoView.renderer)
             }
         }
-    }
-
-    func activeSpeakersDidChange(speakers: [Participant], room _: Room) {
     }
 }
